393e38bb4b8f2d27ca3dbdc3bfc68d6922fd4f86
gengteng <me@gteng.org>
2019-06-27 13:53:10 +0800

    cargo build works when git-fetch-with-cli = true

--- src/util.rs
+++ src/util.rs
@@ -8,16 +8,16 @@ pub fn sock_addr_v4(a: u8, b: u8, c: u8, d: u8, port: u16) -> SocketAddr {
     SocketAddr::new(IpAddr::V4(Ipv4Addr::new(a, b, c, d)), port)
 }
 
-//pub fn get_service_from_query_string(query: &str) -> Option<&str> {
-//    let head = "service=git-";
-//    query.find(head).and_then(|i| {
-//        let start = i + head.len();
-//        match &query[start..].find('&') {
-//            Some(u) => Some(&query[start..u + start]),
-//            None => Some(&query[start..]),
-//        }
-//    })
-//}
+pub fn get_service_from_query_string(query: &str) -> Option<&str> {
+    let head = "service=git-";
+    query.find(head).and_then(|i| {
+        let start = i + head.len();
+        match &query[start..].find('&') {
+            Some(u) => Some(&query[start..u + start]),
+            None => Some(&query[start..]),
+        }
+    })
+}
 
 pub fn no_cache(res: impl Responder) -> impl Responder {
     res.with_header("Expires", "Fri, 01 Jan 1980 00:00:00 GMT")
